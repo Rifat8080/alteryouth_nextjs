a53fc4d3ac7eceb72abbef1a11cda107aab754ac
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2025-01-17 22:44:37 +0600

    icons setup done js icons

--- src/components/schoolEligibility.js
+++ src/components/schoolEligibility.js
@@ -1,4 +1,7 @@
 import React from "react";
+import SvgDisabledFather from "../components/DisabledFather";
+import SvgOrphan from "../components/Orphan";
+import SvgSingleMother from "../components/SingleMother";
 
 const SchoolEligibility = () => {
   const cards = [
@@ -6,34 +9,28 @@ const SchoolEligibility = () => {
       title: "Disabled Father",
       description: "Student's father is physically unable to work",
       svg: (
-        <svg xmlns="http://www.w3.org/2000/svg" width="49.316" height="27.522" viewBox="0 0 49.316 27.522">
-          {/* SVG content here */}
-        </svg>
+        <SvgDisabledFather />
       ),
     },
     {
       title: "Orphan",
       description: "Both parents are deceased or have abandoned the child",
       svg: (
-        <svg xmlns="http://www.w3.org/2000/svg" width="49.316" height="27.522" viewBox="0 0 49.316 27.522">
-          {/* SVG content here */}
-        </svg>
+        <SvgOrphan />
       ),
     },
     {
       title: "Single Mother",
       description: "Student's father is deceased or has abandoned the family",
       svg: (
-        <svg xmlns="http://www.w3.org/2000/svg" width="49.316" height="27.522" viewBox="0 0 49.316 27.522">
-          {/* SVG content here */}
-        </svg>
+       < SvgSingleMother />
       ),
     },
   ];
 
   return (
     <div className="bg-white py-16">
-         <div className="flex flex-col items-center bg-white py-7">
+         <div className="flex flex-col items-center justify-center bg-white py-7">
         <h2 className="text-3xl md:text-4xl font-bold text-center text-black">Scholarship Eligibility</h2>
         <p className="flex text-sm lg:text-base max-w-4xl text-center text-black font-medium">
         Only students who are currently enrolled in Government Primary Schools and fall
@@ -41,7 +38,7 @@ const SchoolEligibility = () => {
         </p>
       </div>
     <div className="container mx-auto py-8 px-4 max-w-7xl ">
-      <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 ">
         {cards.map((card, index) => (
           <div
             key={index}
