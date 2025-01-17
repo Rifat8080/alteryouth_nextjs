0bb4aada934baacd9c7267f25f439a80c1dc3edc
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2025-01-17 17:30:02 +0600

    fixed padding margin

--- src/components/heroSection.js
+++ src/components/heroSection.js
@@ -54,17 +54,17 @@ const HeroSection = () => {
   };
 
   return (
-    <section className="w-full h-screen ">
+    <section className="relative w-full h-full">
       <ToastContainer />
       <div className="absolute inset-0 opacity-96">
         <div className="block lg:hidden">
-          <img src="assets/images/hero_home_phone.jpg" alt="Hero Mobile" className="w-full h-full object-cover relative" />
+          <img src="assets/images/hero_home_phone.jpg" alt="Hero Mobile" className="w-full h-full object-cover" />
         </div>
         <div className="hidden lg:block">
-          <img src="assets/images/hero_pc.jpg" alt="Hero Desktop" className="w-full h-full object-cover " />
+          <img src="assets/images/hero_pc.jpg" alt="Hero Desktop" className="w-full  object-cover" />
         </div>
       </div>
-      <div className="relative z-10 flex items-center justify-center h-full text-white px-8 pt-[350px] md:pt-[950px] lg:pt-0">
+      <div className="relative z-10 flex items-center justify-center h-full text-white px-8 pt-[350px] md:pt-[950px] lg:pt-[110px] xl:pt-[320px] 2xl:pt-[520px]">
         <div className="flex flex-col lg:flex-row items-center justify-between w-full max-w-5xl space-y-8 md:space-y-0 md:space-x-8">
           <div className="flex flex-col space-y-4 text-left max-w-sm">
             <h1 className="text-4xl md:text-6xl font-bold">Start your scholarship</h1>
@@ -85,27 +85,27 @@ const HeroSection = () => {
                 placeholder="Your Email"
                 className="px-4 py-2 border rounded-lg w-full text-black placeholder:text-sm md:placeholder:text-base focus:border-lightGreen focus:ring-lightGreen"
               />
-                <div className="flex items-center border rounded-lg w-full focus-within:border-lightGreen focus-within:ring-lightGreen">
-                  <select
-                    className="px-2 py-2  bg-transparent border-none text-black focus:outline-none"
-                    value={selectedCountry}
-                    onChange={(e) => setSelectedCountry(e.target.value)}
-                  >
-                    {countryCodes.map((country, index) => (
-                      <option key={index} value={country.dial_code}>
-                        {country.flag} {country.dial_code}
-                      </option>
-                    ))}
-                  </select>
-                  <input
-                    type="tel"
-                    placeholder="Your Number"
-                    className="px-0 md:px-4 py-2 border-none rounded-r-lg w-full text-black placeholder:text-sm md:placeholder:text-base focus-within:border-lightGreen focus-within:ring-lightGreen"
-                    pattern="[0-9]*"
-                    value={phoneNumber}
-                    onChange={(e) => setPhoneNumber(e.target.value)}
-                  />
-                </div>
+              <div className="flex items-center border rounded-lg w-full focus-within:border-lightGreen focus-within:ring-lightGreen">
+                <select
+                  className="px-2 py-2 bg-transparent border-none text-black focus:outline-none"
+                  value={selectedCountry}
+                  onChange={(e) => setSelectedCountry(e.target.value)}
+                >
+                  {countryCodes.map((country, index) => (
+                    <option key={index} value={country.dial_code}>
+                      {country.flag} {country.dial_code}
+                    </option>
+                  ))}
+                </select>
+                <input
+                  type="tel"
+                  placeholder="Your Number"
+                  className="px-0 md:px-4 py-2 border-none rounded-r-lg w-full text-black placeholder:text-sm md:placeholder:text-base focus-within:border-lightGreen focus-within:ring-lightGreen"
+                  pattern="[0-9]*"
+                  value={phoneNumber}
+                  onChange={(e) => setPhoneNumber(e.target.value)}
+                />
+              </div>
               <span className="text-gray-700 font-medium">Number of Scholarships</span>
               <div className="flex flex-row items-center justify-between space-x-4">
                 <div className="flex items-center space-x-4">
